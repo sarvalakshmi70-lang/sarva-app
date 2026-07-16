f12e1b11e894c270974a31c0394aaf4926b93390
sarva <sarvalakshmi70@gmail.com>
2026-07-16 16:30:26 +0530

    Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './Navbar'
+import Course from './Course'
+import Footer from './Footer'
+
+function App(){
+    return(
+        <>
+        <Navbar />
+        <Course />
+        <Footer />
+        </>
+
+    );
+}
+
+export default App;
+
+
+
