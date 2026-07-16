1c341352c2e51e5fb292eaf008d558eed14cbf9b
sarva <sarvalakshmi70@gmail.com>
2026-07-16 17:17:33 +0530

    modified react project

---
+++ sarva-app/src/Course.jsx
@@ -0,0 +1,13 @@
+const Course1 ="HTML"
+function Course(){
+    return(
+        <div class="card">
+        <img src="" alt="" />
+        <h3>{Course}</h3>
+        <p>This is Code World HTML Course</p>
+
+        </div>
+    );
+}
+
+export default Course;
